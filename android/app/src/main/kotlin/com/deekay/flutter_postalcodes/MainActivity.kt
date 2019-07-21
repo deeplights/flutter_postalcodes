5af72b54e5521d352426928549efe45bd987a2ea
Deepak Khillare <deepakkhillare@Deepaks-iMac.local>
2019-07-21 10:40:20 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/deekay/flutter_postalcodes/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.deekay.flutter_postalcodes
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
